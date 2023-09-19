3f1264ef0730ec1905a8140a84017a74054064a3
Diego Ferreira <diferodrigues@gmail.com>
2023-09-19 01:09:23 +0000

    Add scripts `lint:check` and `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,14 @@
 function Home() {
-    return <h1>Comprei um barquinho azul ⛴️ para atravessar o oceano 🌊. Chegar do outro lado e dizer que eu tinhamo 😻!!!</h1>
+  return (
+    <h1>
+      Comprei um barquinho azul ⛴️ para atravessar o oceano 🌊. Chegar do outro
+      lado e dizer que eu tinhamo 😻!!!
+    </h1>
+  );
 }
 
-export default Home
+function test() {
+  console.log("fdaçfas");
+}
+
+export default Home;
